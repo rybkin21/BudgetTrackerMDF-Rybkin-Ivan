847e5a0218a5d7db71e7e3bd980626b9d39a3b6c
ivanrybkin <rybkin2517@mail.ru>
2023-06-14 19:27:52 +0300

    Initial Commit

---
+++ BudgetTrackerMDF-Rybkin Ivan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetTrackerMDF-Rybkin Ivan
+//
+//  Created by Ivan Rybkin on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
